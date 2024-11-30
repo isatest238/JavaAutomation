d7e6f983221449499cbfdf9445f3d52bf2eb7400
isatest238 <isatest238@gmail.com>
2024-11-30 21:59:55 +0200

    Masina clasa added

---
+++ src/test/java/VariabilaMetoda/Masina.java
@@ -0,0 +1,35 @@
+package VariabilaMetoda;
+
+import org.testng.annotations.Test;
+
+public class Masina {
+    public String marca;
+    public String model;
+    public Integer anFabricatie;
+
+    @Test
+    public void metodaTest(){
+        dateMasina("Audi","Q3", 2007);
+        dateMasina("BMW","X5", 2000);
+        calculMedie (4.0,6.0);
+    }
+
+    public void dateMasina (String param1, String param2, Integer param3){
+        marca = param1;
+        model = param2;
+        anFabricatie = param3;
+
+        System.out.println("Marca masinii este:" + marca);
+        System.out.println("Modelul masinii este:" + model);
+        System.out.println("An fabricatie masina este:" + anFabricatie);
+    }
+
+    // Calculam media a doua numere
+    public void calculMedie(Double param1, Double param2){
+        Double media = (param1 + param2)/2;
+        System.out.println("Media celor doua numere este:" + media);
+
+    }
+
+}
+
